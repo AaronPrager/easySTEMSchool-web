c4f32f29a0fef8352fd17cf358d04fb9f061e953
Aaron Prager <arik@pragersfamily.com>
2025-07-30 14:25:22 -0400

    Fix nodemailer function name - createTransport instead of createTransporter

--- api/contact.js
+++ api/contact.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     }
 
     // Create transporter using Gmail SMTP
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER, // Your Gmail address
